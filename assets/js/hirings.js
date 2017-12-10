2a071c8ae1557bd495c7d971843c91e063371b66
saadmehmood233 <saadmehmood.me@gmail.com>
2017-12-10 11:49:08 +0500

    Added Count Down

--- assets/js/hirings.js
+++ assets/js/hirings.js
@@ -1,8 +1,33 @@
+var hirings = {};
+
 $.getJSON('http://pucithd.com/data/hirings.json', function(data) {
-  var hirings=data.hirings;
+  hirings=data.hirings;
+  setInterval(countDown, 1000);
+});
+
+function countDown() {
   var str;
+  
   for (var i = 0; i < hirings.length; i++) {
-    str += "<tr><th>"+(i+1)+"</th><td>"+hirings[i].name+"</td><td>"+hirings[i].by+"</td><td>"+hirings[i].campus+"</td><td>"+hirings[i].last_date+"</td><td><a href='"+hirings[i].fb_event+"'>Event Page</a></td><td><a href='"+hirings[i].link+"'>Apply</a></td></tr>";
+    var now = new Date().getTime();
+    var countDownDate = new Date(hirings[i].last_date).getTime();
+
+    var distance = countDownDate - now;
+
+    if(distance > 0 ){
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    } else {
+      var days = 0;
+      var hours = 0;
+      var minutes = 0;
+      var seconds = 0;
+    }
+
+    str += "<tr><th>"+(i+1)+"</th><td>"+hirings[i].name+"</td><td>"+hirings[i].by+"</td><td>"+hirings[i].campus+"</td><td>"+days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s "+"</td><td><a href='"+hirings[i].fb_event+"'>Event Page</a></td><td><a href='"+hirings[i].link+"'>Apply</a></td></tr>";
   }
   $("#hirings").html(JSON.stringify(str));
-});
+}
